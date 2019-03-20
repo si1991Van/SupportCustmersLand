c73a35563442a4f931a70bdc62b6f4d82e3697ff
apple <apple@apples-MacBook-Pro.local>
2019-03-20 17:00:08 +0700

    create project

---
+++ Pods/Target Support Files/Pods-App-Spport-Customers-LandTests/Pods-App-Spport-Customers-LandTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_App_Spport_Customers_LandTests : NSObject
+@end
+@implementation PodsDummy_Pods_App_Spport_Customers_LandTests
+@end
---
+++ Pods/Target Support Files/Pods-App-Spport-Customers-LandUITests/Pods-App-Spport-Customers-LandUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_App_Spport_Customers_LandUITests : NSObject
+@end
+@implementation PodsDummy_Pods_App_Spport_Customers_LandUITests
+@end
